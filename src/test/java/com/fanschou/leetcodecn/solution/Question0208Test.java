4b5c080c72fd1eda7713cb39c4aada891aab4187
fanzhou <runfan.zhou@qq.com>
2022-07-06 10:27:53 +0800

    add solution no.208

---
+++ src/test/java/com/fanschou/leetcodecn/solution/Question0208Test.java
@@ -0,0 +1,20 @@
+package com.fanschou.leetcodecn.solution;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class Question0208Test {
+
+    @Test
+    void testTrie(){
+        Question0208.Trie trie = new Question0208.Trie();
+        trie.insert("apple");
+        assertTrue(trie.search("apple"));
+        assertFalse(trie.search("app"));
+        assertTrue(trie.startsWith("app"));
+        trie.insert("app");
+        assertTrue(trie.search("app"));
+    }
+}
